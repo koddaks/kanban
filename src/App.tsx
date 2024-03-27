0db400578b567f91c59564acf8c16e289f3cbe29
koddaks <koss199710@gmail.com>
2024-03-27 19:31:53 +0200

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='libraries'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
