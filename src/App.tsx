f1ff8895ee00a02ca8bbb1b8cc217ae1843d72fa
koddaks <koss199710@gmail.com>
2024-03-27 18:11:46 +0200

    add component to project with shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +21,8 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button>  
+        <Button>shadcn-ui@latest add button</Button>   
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
