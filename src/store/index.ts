c6702a813e1ab786b1a9f0c20a721bf76ebbdb1a
koddaks <koss199710@gmail.com>
2024-03-27 20:42:14 +0200

    add devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+
 
 interface StoreState {
   count: number
   increaseCount: (by: number) => void
 }
 
-const useStore = create<StoreState>()((set) => ({
-  count: 0,
-  increaseCount: (by) => set((state) => ({ count: state.count + by })),
-}))
+const useStore = create<StoreState>()(devtools((set) => ({
+    count: 0,
+    increaseCount: (by) => set((state) => ({ count: state.count + by })),
+  })))
 
 export default useStore
