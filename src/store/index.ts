f0638febd2b2be9c206c261b76e5b895123d8fb2
koddaks <koss199710@gmail.com>
2024-03-27 23:33:21 +0200

    test fetch repo data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,44 @@
+
+import { Issue, SearchState } from '@/types'
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
-
 
 interface StoreState {
   count: number
   increaseCount: (by: number) => void
+  fetchRepoData: (repoUrl: string, searchState: SearchState) => Promise<void>
+  repoData: Issue[]
 }
 
-const useStore = create<StoreState>()(devtools((set) => ({
+const useStore = create<StoreState>()(
+  devtools((set) => ({
     count: 0,
+    repoData: [],
     increaseCount: (by) => set((state) => ({ count: state.count + by })),
-  })))
+    async fetchRepoData(repoUrl: string, searchState: SearchState) {
+      try {
+        const queryParams = new URLSearchParams({
+          direction: 'desc',
+          per_page: '100',
+          state: `${searchState}`,
+        })
+        const url = `${repoUrl}/issues?q=${queryParams}`
+
+        const response = await fetch(url, {
+          headers: {
+            accept: 'application/vnd.github+json',
+          },
+        })
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data: Issue[] = await response.json()
+        set({ repoData: data })
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    },
+  }))
+)
 
 export default useStore
