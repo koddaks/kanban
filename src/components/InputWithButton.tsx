9cc0121d5eff8531144ad07348d4c8a11fc4d185
koddaks <koss199710@gmail.com>
2024-03-31 21:09:49 +0300

    Receiving Issues by entering the repository address in the input

--- src/components/InputWithButton.tsx
+++ src/components/InputWithButton.tsx
@@ -1,11 +1,28 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import useIssuesStore from '@/store'
 
-export function InputWithButton() {
+
+import { useRef } from 'react'
+
+
+export function InputWithButton() { 
+
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+
+  const getIssues = useIssuesStore((state) => state.getAllIssues)
+  const handleGetIssues = (): void => {
+    const inputValue = inputRef?.current?.value;
+    if (inputValue !== undefined && inputValue !== '') {
+      getIssues(inputValue);
+    }
+  };
+ 
   return (
     <div className="m-0 mx-auto flex w-full items-center justify-center space-x-2 pb-8">
-      <Input className="max-w-96" type="search" placeholder="Enter repository URL" />
-      <Button type="submit">Load issues</Button>
+      <Input className="max-w-96" ref={inputRef} type="search" placeholder="Enter repository URL" />
+      <Button onClick={handleGetIssues} type="submit">Load issues</Button>
     </div>
   )
 }
