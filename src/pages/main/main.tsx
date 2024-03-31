ded06d2a2f405ad0a3618887dfb8a8b22b6c0ca0
koddaks <koss199710@gmail.com>
2024-03-31 17:22:57 +0300

    make main component

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,12 @@
 import { ColumnContainer } from '@/components/ColumnContainer'
+import { InputWithButton } from '@/components/InputWithButton'
 import useStore from '@/store/index'
 import { Issue, IssueState } from '@/types'
 
 export function Main() {
-  const repoData = useStore((state) => state.repoData)
+  const issues = useStore((state) => state.issues)
 
-  const todoIssues = repoData.filter((issue: Issue) => {
+  const todoIssues = issues.filter((issue: Issue) => {
     const currentDate = Date.now()
     const issueDate = new Date(issue.created_at).getTime()
     const threeDaysAgo = currentDate - 3 * 24 * 60 * 60 * 1000
@@ -13,17 +14,18 @@ export function Main() {
     return issueDate >= threeDaysAgo && issue.state !== IssueState.Closed ? issue : null
   })
 
-  const InProgressIssues = repoData.filter((issue) => IssueState.Open && issue.assignee !== null)
+  const InProgressIssues = issues.filter((issue) => IssueState.Open && issue.assignee !== null)
 
-  const doneIssues = repoData.filter((issue) => issue.state === IssueState.Closed)
+  const doneIssues = issues.filter((issue) => issue.state === IssueState.Closed)
 
   return (
-    <>
-      <div className="flex w-full border-collapse gap-4 rounded-lg border-2 border-solid border-indigo-600 bg-slate-300 p-[20px]">
-        <ColumnContainer column="ToDo" issues={todoIssues} />
-        <ColumnContainer column="In Progress" issues={InProgressIssues} />
-        <ColumnContainer column="Done" issues={doneIssues} />
-      </div>
-    </>
+    <main>
+      <InputWithButton />      
+        <div className="flex border-collapse gap-4 rounded-lg border-2 border-solid border-indigo-600 bg-slate-300 p-[20px]">
+          <ColumnContainer column="ToDo" issues={todoIssues} />
+          <ColumnContainer column="In Progress" issues={InProgressIssues} />
+          <ColumnContainer column="Done" issues={doneIssues} />
+        </div>    
+    </main>
   )
 }
