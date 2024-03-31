0b58dfc68908f3642f7f5ede64be2c695839be49
koddaks <koss199710@gmail.com>
2024-03-31 17:16:19 +0300

    delete commnents

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import { useEffect } from 'react'
 import { IssueState } from './types'
 import { Main } from './pages/main/main'
 
+const API_URL = 'https://api.github.com/repos/facebook/react'
+
 function App() {
-  const repoUrl = 'https://api.github.com/repos/facebook/react'
-  const fetchRepoData = useStore((state) => state.fetchRepoData)
-  const repoData = useStore((state) => state.repoData)
-  console.log(repoData)
+  
+  // const repoUrl = 'https://api.github.com/repos/Georgegriff/react-dnd-kit-tailwind-shadcn-ui'
+  // const repoUrl = 'https://api.github.com/repos/vuejs/vue'
+
+
+  const fetchIssuesData = useStore((state) => state.fetchIssuesData)
+  const issues = useStore((state) => state.issues)
+  console.log(issues);
 
   useEffect(() => {
-    fetchRepoData(repoUrl, IssueState.All)
-  }, [repoUrl])
+    fetchIssuesData(API_URL, IssueState.All)
+  }, [API_URL])
 
   return <Main />
 }
