a9b5a8cd90c9493d6395e0182a9d33f7f1d2684d
koddaks <koss199710@gmail.com>
2024-03-31 21:12:29 +0300

    Removed unnecessary data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import './App.css'
-import useStore from './store'
-import { useEffect } from 'react'
-import { IssueState } from './types'
 import { Main } from './pages/main/main'
 
-const API_URL = 'https://api.github.com/repos/facebook/react'
-
 function App() {
-  const getIssues = useStore((state) => state.getIssues)
-  const issues = useStore((state) => state.issues)
-  console.log(issues)
-
-  useEffect(() => {
-    getIssues(API_URL, IssueState.All)
-  }, [API_URL])
 
   return <Main />
 }
