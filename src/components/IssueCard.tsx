ce6c10032de7dcf72e1035b267919bbbd2615ab6
koddaks <koss199710@gmail.com>
2024-03-31 21:11:33 +0300

    data destructuring

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -3,29 +3,31 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { getTimeStringSinceIssueOpened } from '@/lib/utils'
 
 type IssueCardProps = {
-  data: Issue
+  issue: Issue
 }
 
-export function IssueCard({ data }: IssueCardProps) {
+export function IssueCard({ issue }: IssueCardProps) {
+  const { html_url, title, number, created_at, state, body } = issue
+  const { login, type, avatar_url } = issue.user
+
   return (
     <Card className="flex max-w-[360px] flex-col items-center bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 duration-500 hover:scale-105 hover:from-pink-500 hover:to-yellow-500">
       <CardHeader>
-        <a href={data.html_url} target="_blank" rel="noopener noreferrer">
-          <CardTitle>{data.title}</CardTitle>
+        <a href={html_url} target="_blank" rel="noopener noreferrer">
+          <CardTitle>{title}</CardTitle>
           <CardDescription>
-            #{data.number} opened {getTimeStringSinceIssueOpened(data.created_at)}
+            #{number} opened {getTimeStringSinceIssueOpened(created_at)}
           </CardDescription>
         </a>
         <CardDescription className="flex flex-col items-center">
-          Opened by {data.user.login} | {data.user.type}{' '}
-          <img className="size-16 rounded-full" src={data.user.avatar_url} />
+          Opened by {login} | {type} <img className="size-16 rounded-full" src={avatar_url} />
         </CardDescription>
       </CardHeader>
       <CardContent className="max-w-96 overflow-visible">
-        <p className="truncate">{data.body}</p>
+        <p className="truncate">{body}</p>
       </CardContent>
       <CardFooter className="justify-center">
-        <p className="rounded-lg bg-sky-400/100 p-1">{data.state.toUpperCase()}</p>
+        <p className="rounded-lg bg-sky-400/100 p-1">{state.toUpperCase()}</p>
       </CardFooter>
     </Card>
   )
