5ca216d6d96e02e268e88024f1744b48b552658e
koddaks <koss199710@gmail.com>
2024-04-09 13:06:38 +0300

    delete comments

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,36 +1,12 @@
 import { BreadCrumbs } from '@/components/BreadCrumbs'
-import { ColumnContainer } from '@/components/ColumnContainer'
 import { InputWithButton } from '@/components/InputWithButton'
 import { KanbanBoard } from '@/components/KabanBoard'
-import useIssuesStore from '@/store/index'
-
 import KanbanBoardDnD from "@/components/DndTest/KanbanBoardDnD";
 
 
-// import { useEffect } from 'react'
+
 
 export function Main() {
-  // const issues = useIssuesStore((state) => state.all)
-  // const todoIssues = useIssuesStore((state) => state.openedWithAssignee)
-  // const InProgressIssues = useIssuesStore((state) => state.inProgress)
-  // const doneIssues = useIssuesStore((state) => state.closed)
-
-  // const getTodoIssues = useIssuesStore((state) => state.getOpenedIssuesWithAssignee)
-  // const getInProgressIssues = useIssuesStore((state) => state.getInProgressIssues)
-  // const geDoneIssues = useIssuesStore((state) => state.getClosedIssues)
-
-  // useEffect(() => {
-  //   getTodoIssues(issues)
-  //   getInProgressIssues(issues)
-  //   geDoneIssues(issues)
-  // }, [])
-
-  // useEffect(() => {
-  //   getTodoIssues(issues)
-  //   getInProgressIssues(issues)
-  //   geDoneIssues(issues)
-  // }, [issues])
-
   return (
     <main>
       <InputWithButton />
