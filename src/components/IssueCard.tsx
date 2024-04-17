1e6ad9a3857a57b2d2f9a8b204d8dcad3259fa2a
koddaks <koss199710@gmail.com>
2024-04-17 13:24:35 +0300

    refactor IssueCard ui

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -1,9 +1,11 @@
 import { Issue } from '@/types'
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+import { Card, CardDescription, CardFooter, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { cn } from '@/lib/utils'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { getTimeStringSinceIssueOpened } from '@/utils'
+import { Link } from 'lucide-react'
 
 type IssueCardProps = {
   issue: Issue
@@ -12,6 +14,7 @@ type IssueCardProps = {
 export function IssueCard({ issue }: IssueCardProps) {
   const { html_url, title, number, created_at, state } = issue
   const { login, type, avatar_url } = issue.user
+  const userUrl = issue.user.html_url
 
   const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
     id: issue.id,
@@ -33,25 +36,33 @@ export function IssueCard({ issue }: IssueCardProps) {
       {...attributes}
       {...listeners}
       className={cn(
-        `xl:max-w-80 lg:max-w-72 md:max-w-56 sm:max-w-52 break-all w-4/5 ${
-          isDragging ? 'cursor-grab border-2 border-rose-500 opacity-30' : ''
-        }`
+        `w-full break-all sm:max-w-52 md:max-w-56 lg:max-w-72 xl:max-w-80 cursor-grab  ${isDragging ? ' border-2 border-rose-500 opacity-30' : ''}`
       )}
     >
-      <CardHeader>
-        <a href={html_url} target="_blank" rel="noopener noreferrer">
-          <CardTitle className="pb-2 text-balance">{title}</CardTitle>
-          <CardDescription>
-            #{number} opened {getTimeStringSinceIssueOpened(created_at)}
-          </CardDescription>
-        </a>
-        <CardDescription className='flex flex-col items-center gap-2'>
-          Opened by {login} | {type} <img className="size-16 rounded-full" src={avatar_url} />
-        </CardDescription>
+      <CardHeader className="flex flex-col items-center justify-between gap-1 space-y-0 pb-2">
+        <CardTitle className="line-clamp-2 text-balance text-center px-2 text-xs font-medium md:text-sm">
+          <a href={html_url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>          
+        </CardTitle>
+        <p className="text-xs">Opened by {login} | {type}</p>
+        <div className=" w-1/3 flex flex-col items-center gap-1">
+          <Avatar className="size-8 rounded-full text-muted-foreground md:size-12">
+            <AvatarImage src={avatar_url} alt="avatar image" />
+            <AvatarFallback>{userUrl}</AvatarFallback>
+          </Avatar>
+          <p className="text-[10px] leading-4 font-bold">{login}</p>
+        </div>
       </CardHeader>
-      <CardFooter className='flex flex-col items-center'>
-        <p className=' w-1/3 bg-sky-400/100 rounded-md p-1'>{state.toUpperCase()}</p>
-      </CardFooter>
+      <CardContent className="flex flex-col gap-2 items-center">
+        <div className="flex flex-col items-center text-xs font-bold xl:text-sm">
+          #{number} created {getTimeStringSinceIssueOpened(created_at)}
+        </div>
+        <p className="text-xs text-muted-foreground md:text-base">{state}</p>
+      </CardContent>
     </Card>
   )
 }
+
+
+
