62f70b793f1213fae88376ed92fdc01f284de4a1
koddaks <koss199710@gmail.com>
2024-04-23 21:53:06 +0300

    fix function to extendIssuesWithStatus

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
-import { Issue, IssueState } from '@/types'
+
+import { Issue, IssueStatus, ResponseIssue } from '@/types/issues'
 
 export function getTimeStringSinceIssueOpened(createdAt: string): string {
   const postDate = new Date(createdAt)
@@ -13,54 +14,20 @@ export function getTimeStringSinceIssueOpened(createdAt: string): string {
   return `${differenceInDays} ${dayWord} ago`
 }
 
-export function getOpenedIssuesWithAssignee(issues: Issue[]): Issue[] {
-  const currentDate = Date.now()
-  const threeDaysAgo = currentDate - 3 * 24 * 60 * 60 * 1000
+export function extendIssuesWithStatus(issues: ResponseIssue[]): Issue[] {
+  return issues.map((issue) => {
+    let status: IssueStatus
 
-  return issues.filter((issue: Issue) => {
-    const issueDate = new Date(issue.created_at).getTime()
-    return issueDate >= threeDaysAgo && issue.state !== IssueState.Closed && !issue.assignee
+    if (issue.state === 'closed') {
+      status = 'done'
+    } else if (issue.assignee) {
+      status = 'in-progress'
+    } else {
+      status = 'todo'
+    }
+
+    return { ...issue, status }
   })
-}
-
-export function getClosedIssues(issues: Issue[]): Issue[] {
-  return issues.filter((issue: Issue) => issue.state === IssueState.Closed)
-}
-
-export function getInProgressIssues(issues: Issue[]): Issue[] {
-  return issues.filter((issue: Issue) => issue.state === IssueState.Open && issue.assignee !== null)
-}
-
-export function sortIssuesByColumn(issues: Issue[], column: string | number): Issue[] {
-  let sortedIssues: Issue[] = []
-
-  switch (column) {
-    case 'todo':
-      sortedIssues = getOpenedIssuesWithAssignee(issues).map((issue) => ({
-        ...issue,
-        columnId: 'todo',
-      }))
-      break
-    case 'done':
-      sortedIssues = getClosedIssues(issues).map((issue) => ({
-        ...issue,
-        columnId: 'done',
-      }))
-      break
-    case 'doing':
-      sortedIssues = getInProgressIssues(issues).map((issue) => ({
-        ...issue,
-        columnId: 'doing',
-      }))
-      break
-    default:
-      sortedIssues = issues.map((issue) => ({
-        ...issue,
-        columnId: 'all',
-      }))
-  }
-
-  return sortedIssues
 }
 
 export function extractOwnerAndRepo(url: string) {
