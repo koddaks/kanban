336533e930da056cf869748a9913acd9949eafc7
koddaks <koss199710@gmail.com>
2024-04-23 22:50:08 +0300

    change className

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -6,9 +6,6 @@ import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { getTimeStringSinceIssueOpened } from '@/utils'
 
-
-
-
 type IssueCardProps = {
   issue: Issue
 }
@@ -37,26 +34,28 @@ export function IssueCard({ issue }: IssueCardProps) {
       style={style}
       {...attributes}
       {...listeners}
-      className={cn(
-        `w-full break-all sm:max-w-52 md:max-w-56 lg:max-w-72 xl:max-w-80 cursor-grab  ${isDragging ? ' border-2 border-rose-500 opacity-30' : ''}`
+      className={cn('w-full', 'break-all', 'sm:max-w-52', 'md:max-w-56', 'lg:max-w-72', 'xl:max-w-80', 'cursor-grab',
+        { 'border-2 border-rose-500 opacity-30': isDragging }
       )}
     >
       <CardHeader className="flex flex-col items-center justify-between gap-1 space-y-0 pb-2">
-        <CardTitle className="line-clamp-2 text-balance text-center px-2 text-xs font-medium md:text-sm">
+        <CardTitle className="line-clamp-2 text-balance px-2 text-center text-xs font-medium md:text-sm">
           <a href={html_url} target="_blank" rel="noopener noreferrer">
             {title}
-          </a>          
+          </a>
         </CardTitle>
-        <p className="text-xs">Opened by {issue?.user?.login} | {issue?.user?.type}</p>
-        <div className=" w-1/3 flex flex-col items-center gap-1">
+        <p className="text-xs">
+          Opened by {issue?.user?.login} | {issue?.user?.type}
+        </p>
+        <div className=" flex w-1/3 flex-col items-center gap-1">
           <Avatar className="size-8 rounded-full text-muted-foreground md:size-12">
             <AvatarImage src={issue?.user?.avatar_url} alt="avatar image" />
             <AvatarFallback>{userUrl}</AvatarFallback>
           </Avatar>
-          <p className="text-[10px] leading-4 font-bold">{issue?.user?.login}</p>
+          <p className="text-[10px] font-bold leading-4">{issue?.user?.login}</p>
         </div>
       </CardHeader>
-      <CardContent className="flex flex-col gap-2 items-center">
+      <CardContent className="flex flex-col items-center gap-2">
         <div className="flex flex-col items-center text-xs font-bold xl:text-sm">
           #{number} created {getTimeStringSinceIssueOpened(created_at)}
         </div>
@@ -65,6 +64,3 @@ export function IssueCard({ issue }: IssueCardProps) {
     </Card>
   )
 }
-
-
-
