8e857130009156446fd3cafa3ca8ffdd1fdb03de
koddaks <koss199710@gmail.com>
2024-04-23 22:22:46 +0300

    rebuild breadcrumbs

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -33,58 +33,81 @@ export function BreadCrumbs() {
     }
   }, [currentRepoUrl])
 
-
-
   function handleSetCurrentRepoUrl(link: string) {
     setCurrentRepoUrl(link)
   }
 
-  const owners = repoList.reduce<Record<string, RepoInfo[]>>((acc, curr) => {
-    if (!acc[curr.owner]) {
-      acc[curr.owner] = []
+  const owners = Object.keys(
+    repoList.reduce<Record<string, RepoInfo[]>>((acc, curr) => {
+      if (!acc[curr.owner]) {
+        acc[curr.owner] = []
+      }
+      acc[curr.owner].push(curr)
+      return acc
+    }, {})
+  )
+
+
+  const [filteredRepos, setFilteredRepos] = useState<RepoInfo[]>([])
+
+ 
+  useEffect(() => {
+    if (activeOwner) {      
+      const reposForOwner = repoList.filter((repo) => repo.owner === activeOwner)    
+      
+      setCurrentRepoUrl(reposForOwner[0].repoUrl)
+      setFilteredRepos(reposForOwner)      
+      setActiveRepo(reposForOwner[0].repo)
+      
     }
-    acc[curr.owner].push(curr)
-    return acc
-  }, {})
+  }, [repoList, activeOwner])
 
   return (
     <Breadcrumb>
-      <BreadcrumbList>        
-        {Object.keys(owners).map((owner) => (
-          <React.Fragment key={owner}>
-            <BreadcrumbItem className={activeOwner === owner ? 'text-blue-700' : ''}>
-              <DropdownMenu>
-                <DropdownMenuTrigger type="button" className="flex items-center gap-1">
-                  {owner}
-                  <ChevronDown className="h-4 w-4" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  {owners[owner].map((repo) => (
-                    <DropdownMenuItem
-                      key={repo.repoUrl}                     
-                      className={
-                        activeOwner === owner && activeRepo === repo.repo ? 'bg-slate-300 rounded-sm' : ''
-                      }
-                      onClick={() => handleSetCurrentRepoUrl(repo.repoUrl)}
-                    >
-                      {repo.repo}
-                    </DropdownMenuItem>
-                  ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator>
-              <Slash />
-            </BreadcrumbSeparator>
-          </React.Fragment>
-        ))}
+      <BreadcrumbList>
         <BreadcrumbItem>
-          <BreadcrumbLink href={ownerAndRepoData?.ownerUrl} target='_blank'>Link to owner: <span className="text-blue-600">{ownerAndRepoData?.owner}</span></BreadcrumbLink>
-          <BreadcrumbSeparator>
+          <DropdownMenu>
+            <DropdownMenuTrigger type="button" className="flex items-center gap-1">
+              {activeOwner}
+              <ChevronDown className="h-4 w-4" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {owners.map((owner) => (
+                <DropdownMenuItem
+                  key={owner}
+                  className={activeOwner === owner ? 'rounded-sm bg-slate-300' : ''}
+                  onClick={() => setActiveOwner(owner)}
+                >
+                  {owner}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
           <Slash />
         </BreadcrumbSeparator>
-          <BreadcrumbLink href={ownerAndRepoData?.repoUrl} target='_blank'>Link to repository: <span className="text-blue-600">{ownerAndRepoData?.repo}</span></BreadcrumbLink>
-        </BreadcrumbItem>
+        {activeOwner && (
+          <BreadcrumbItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger type="button" className="flex items-center gap-1">
+                {activeRepo}
+                <ChevronDown className="h-4 w-4" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {filteredRepos.map((repo) => (
+                  <DropdownMenuItem
+                    key={repo.repoUrl}
+                    className={activeRepo === repo.repo ? 'rounded-sm bg-slate-300' : ''}
+                    onClick={() => handleSetCurrentRepoUrl(repo.repoUrl)}
+                  >
+                    {repo.repo}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </BreadcrumbItem>
+        )}
       </BreadcrumbList>
     </Breadcrumb>
   )
