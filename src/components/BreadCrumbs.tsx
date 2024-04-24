37987bf6eb9ac0287b293eeb9776d6e4e1c3bb2a
koddaks <koss199710@gmail.com>
2024-04-24 21:55:27 +0300

    make links to owner and repo

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -1,6 +1,6 @@
 import {
   Breadcrumb,
-  BreadcrumbItem,  
+  BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
@@ -14,98 +14,112 @@ import useIssuesStore from '@/store'
 import { RepoInfo } from '@/types'
 import { extractOwnerAndRepo } from '@/utils'
 
-import { ChevronDown, Slash } from 'lucide-react'
+import { ChevronDown, Link, Slash } from 'lucide-react'
 import { useEffect, useState } from 'react'
+import { Button } from './ui/button'
 
+export function BreadCrumbs({ currentRepoUrl }: { currentRepoUrl: string }) {
+  const setCurrentRepoUrl = useIssuesStore((state) => state.setCurrentRepoUrl)
+  const [activeOwner, setActiveOwner] = useState<string>('')
+  const [activeRepo, setActiveRepo] = useState<string>('')
 
-export function BreadCrumbs({currentRepoUrl}: {currentRepoUrl: string}) {
-  const setCurrentRepoUrl = useIssuesStore((state) => state.setCurrentRepoUrl);
-  const [activeOwner, setActiveOwner] = useState<string>('');
-  const [activeRepo, setActiveRepo] = useState<string>('');
+  const repoList = useIssuesStore((state) => state.repoList)
+  const currentRepoInfo = extractOwnerAndRepo(currentRepoUrl)
 
-  const repoList = useIssuesStore((state) => state.repoList);
-  const currentRepoInfo = extractOwnerAndRepo(currentRepoUrl);
+  useEffect(() => {
+    if (currentRepoInfo) {
+      setActiveOwner(currentRepoInfo?.owner)
+      setActiveRepo(currentRepoInfo?.repo)
+    }
+  }, [currentRepoUrl])
 
-  useEffect(() => {    
-    if(currentRepoInfo) {
-      setActiveOwner(currentRepoInfo?.owner);
-      setActiveRepo(currentRepoInfo?.repo);
-    }     
-    
-  }, [currentRepoUrl]);
-
-  const issuesByOwner: Record<string, Record<string, RepoInfo>> = {};
+  const issuesByOwner: Record<string, Record<string, RepoInfo>> = {}
   repoList.forEach((repo) => {
     if (!issuesByOwner[repo.owner]) {
-      issuesByOwner[repo.owner] = {};
+      issuesByOwner[repo.owner] = {}
     }
-    issuesByOwner[repo.owner][repo.repo] = repo;
-  });
+    issuesByOwner[repo.owner][repo.repo] = repo
+  })
 
   const handleSetCurrentRepo = (selectedRepo: string) => {
-    setActiveRepo(selectedRepo);
-    const repoInfo = issuesByOwner[activeOwner][selectedRepo];
+    setActiveRepo(selectedRepo)
+    const repoInfo = issuesByOwner[activeOwner][selectedRepo]
     if (repoInfo) {
-      setCurrentRepoUrl(repoInfo.repoUrl);
+      setCurrentRepoUrl(repoInfo.repoUrl)
     }
-  };
+  }
 
   const handleSetCurrentOwner = (selectedOwner: string) => {
-    setActiveOwner(selectedOwner);
-    const reposForOwner = repoList.filter((repo) => repo.owner === selectedOwner);
+    setActiveOwner(selectedOwner)
+    const reposForOwner = repoList.filter((repo) => repo.owner === selectedOwner)
     if (reposForOwner.length > 0) {
-      setActiveRepo(reposForOwner[0].repo);
-      setCurrentRepoUrl(reposForOwner[0].repoUrl);
+      setActiveRepo(reposForOwner[0].repo)
+      setCurrentRepoUrl(reposForOwner[0].repoUrl)
     }
-  };
+  }
 
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger type="button" className="flex items-center gap-1">
-              {activeOwner}
-              <ChevronDown className="h-4 w-4" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              {Object.keys(issuesByOwner).map((owner) => (
-                <DropdownMenuItem
-                  key={owner}
-                  className={activeOwner === owner ? 'rounded-sm bg-slate-300' : ''}
-                  onClick={() => handleSetCurrentOwner(owner)}
-                >
-                  {owner}
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator>
-          <Slash />
-        </BreadcrumbSeparator>
-        {activeOwner && (
+    <div className="flex flex-row justify-between items-center">
+      <Breadcrumb>
+        <BreadcrumbList>
           <BreadcrumbItem>
             <DropdownMenu>
               <DropdownMenuTrigger type="button" className="flex items-center gap-1">
-                {activeRepo}
+                {activeOwner}
                 <ChevronDown className="h-4 w-4" />
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                {Object.keys(issuesByOwner[activeOwner]).map((repo) => (
+                {Object.keys(issuesByOwner).map((owner) => (
                   <DropdownMenuItem
-                    key={repo}
-                    className={activeRepo === repo ? 'rounded-sm bg-slate-300' : ''}
-                    onClick={() => handleSetCurrentRepo(repo)}
+                    key={owner}
+                    className={activeOwner === owner ? 'rounded-sm bg-slate-300' : ''}
+                    onClick={() => handleSetCurrentOwner(owner)}
                   >
-                    {repo}
+                    {owner}
                   </DropdownMenuItem>
                 ))}
               </DropdownMenuContent>
             </DropdownMenu>
           </BreadcrumbItem>
-        )}
-      </BreadcrumbList>
-    </Breadcrumb>
-  );
+          <BreadcrumbSeparator>
+            <Slash />
+          </BreadcrumbSeparator>
+          {activeOwner && (
+            <BreadcrumbItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger type="button" className="flex items-center gap-1">
+                  {activeRepo}
+                  <ChevronDown className="h-4 w-4" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  {Object.keys(issuesByOwner[activeOwner]).map((repo) => (
+                    <DropdownMenuItem
+                      key={repo}
+                      className={activeRepo === repo ? 'rounded-sm bg-slate-300' : ''}
+                      onClick={() => handleSetCurrentRepo(repo)}
+                    >
+                      {repo}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </BreadcrumbItem>
+          )}
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="flex w-1/3 flex-row gap-2">
+        <Button className="w-1/2 h-6" asChild>
+          <a href={currentRepoInfo?.ownerUrl} target="_blank">
+            {currentRepoInfo?.owner}
+          </a>
+        </Button>
+        <Button className="w-1/2 h-6" asChild>
+          <a href={currentRepoInfo?.repoUrl} target="_blank">
+            {currentRepoInfo?.repo}
+          </a>
+        </Button>
+      </div>
+    </div>
+  )
 }
